04479f0867c4c84e150b5aa8b94fa438b6ee0acf
Andrei Korigodski <akorigod@gmail.com>
2019-03-22 06:45:36 +0300

    schematics: decouple MCU and use red-green LEDs

--- schematics/card.py
+++ schematics/card.py
@@ -9,38 +9,62 @@ sk.lib_search_paths[sk.KICAD].extend([
     'components'
 ])
 
+
+class Cap(Part):
+    def __init__(self, capacitance: str = '0.1uF', footprint: str = 'C_0603', *args, **kwargs):
+        footprint_name = footprint if ':' in footprint else f'Capacitor_SMD:{footprint}'
+        super().__init__(
+            'Device', 'C', value=capacitance, footprint=footprint_name, *args, **kwargs
+        )
+
+
+class Res(Part):
+    def __init__(self, resistance: str, footprint: str = 'R_0603_1608Metric', *args, **kwargs):
+        footprint_name = footprint if ':' in footprint else f'Resistor_SMD:{footprint}'
+        super().__init__(
+            'Device', 'R', value=resistance, footprint=footprint_name, *args, **kwargs
+        )
+
+
+def efm_decouple(efm, vdd, gnd):
+    efm['DECOUPLE'] & Cap('1uF', 'C_0603') & gnd
+
+    for pin in efm['IOVDD_[0-9]+']:
+        vdd & Cap('0.1uF', 'C_0603', description=f'{pin} decoupling cap') & gnd
+    vdd & Cap('10uF', 'C_0603', description=f'IOVDD decoupling cap') & gnd
+
+    avdd = Net('MCU_AVDD')
+    for pin in efm['AVDD_[0-9]+']:
+        avdd & Cap('10nF', 'C_0603', description=f'{pin} decoupling cap') & gnd
+    avdd & Cap('10uF', 'C_0603', description=f'AVDD decoupling cap') & gnd
+    (
+        vdd &
+        Part('Device', 'Ferrite_Bead_Small', footprint='Inductor_SMD:L_0603_1608Metric') &
+        Res('1') &
+        avdd
+    )
+
+    (
+        vdd & (
+            Cap('0.1uF', description='VDD decoupling cap') |
+            Cap('3.3uF', description='VDD decoupling cap') |
+            Cap('100uF', description='VDD decoupling cap')
+        ) & gnd
+    )
+
+
+vdd = Net('VDD')
+gnd = Net('GND')
+
 coin_battery = Part('Device', 'Battery_Cell', footprint='Battery_Holders:3028')
 mcu = Part('EFM32LG232F64', 'EFM32LG232F64', footprint='Package_QFP:LQFP-64_10x10mm_P0.5mm')
 imu = Part('Sensor_Motion', 'LSM6DS3', footprint='LSM6D3:LSM6D3')
-# led_dc_converter = Part('charge_pumps', 'CAT3661', footprint='Package_DFN_QFN:QFN-16-1EP_3x3mm_P0.5mm_EP1.8x1.8mm')
+leds = 49 * Part('Device', 'LED_DUAL_AACC', TEMPLATE, footprint='LED_DUAL_0606')
 
-leds = 49 * Part('led_rgba', 'LED_RGBA', TEMPLATE)
-
-
-@subcircuit
-def high_side_switch(control_in, load_power_out, vcc, gnd):
-    driving_mosfet = Part('Device', 'Q_NMOS_DSG')
-    driving_mosfet_pulldown = Part('Device', 'R', value='220K')
-
-    power_mosfet = Part('Device', 'Q_PMOS_DSG')
-    power_mosfet_pullup = Part('Device', 'R', value='22K')
-
-    driving_mosfet['G'] += control_in
-    driving_mosfet['G'] & driving_mosfet_pulldown & gnd
-    driving_mosfet['S'] += gnd
-
-    (
-        (vcc | power_mosfet['S']) &
-        power_mosfet_pullup &
-        (driving_mosfet['D'] | power_mosfet['G'])
-    )
-    power_mosfet['D'] += load_power_out
-
-
-gnd = Net('GND')
-
-sw = high_side_switch(Net(), Net(), Net(), gnd)
-
+coin_battery['+'] += vdd
 coin_battery['-'] += gnd
+
+efm_decouple(mcu, vdd, gnd)
+mcu['VSS'] += gnd
 
 generate_netlist()
