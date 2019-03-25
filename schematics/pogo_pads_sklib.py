187c349ffa389d00e1ffe0ccddea565a4ef169f4
Andrei Korigodski <akorigod@gmail.com>
2019-03-25 22:30:01 +0300

    schematics: add debug and programming interface

---
+++ schematics/pogo_pads_sklib.py
@@ -0,0 +1,19 @@
+from skidl import Pin, Part, SchLib, SKIDL, TEMPLATE
+
+SKIDL_lib_version = '0.0.1'
+
+pogo_pads_lib = SchLib(tool=SKIDL).add_parts(*[
+    Part(
+        name='POGO_10PADS',
+        dest=TEMPLATE,
+        tool=SKIDL,
+        description='Single capacitive sense touch pad',
+        keywords='capacitive sense touch pad',
+        ref_prefix='TS',
+        do_erc=True,
+        pins=[
+            Pin(num=i, name=f'PAD{i}', func=Pin.PASSIVE, do_erc=True)
+            for i in range(1, 11)
+        ]
+    )
+])
