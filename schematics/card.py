28bb4c67903ff1536e94fc4ede433d12fba29253
Andrei Korigodski <akorigod@gmail.com>
2019-03-29 19:04:13 +0300

    footprints: generate touch slider with my new python tool

--- schematics/card.py
+++ schematics/card.py
@@ -146,7 +146,7 @@ lfxo = Part('Device', 'Crystal', value='32768Hz', footprint='Crystal:Crystal_SMD
 imu = Part('Sensor_Motion', 'LSM6DS3', footprint='footprints:LSM6DS3')
 led_template = Part('Device', 'LED_DUAL_AACC', TEMPLATE, footprint='footprints:LED_DUAL_0606')
 touch_pads = 4 * Part(touch_sense_lib, 'TOUCH_PAD', TEMPLATE, footprint='footprints:TouchPad_D8.0mm')
-touch_slider = Part(touch_sense_lib, 'TOUCH_SLIDER_4PADS', footprint='footprints:TouchSlider-4_50x8mm')
+touch_slider = Part(touch_sense_lib, 'TOUCH_SLIDER_4PADS', footprint='footprints:TouchSlider-4_40x7mm')
 pogo_pads = Part(pogo_pads_lib, 'POGO_10PADS', footprint='')
 
 vdd = Net('VDD')
