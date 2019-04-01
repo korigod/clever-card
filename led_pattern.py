9f5342fc09ddf4e1c9e528ec4fbd93ed3fb33010
Andrei Korigodski <akorigod@gmail.com>
2019-04-01 18:33:30 +0300

    led_pattern: improve readability

--- led_pattern.py
+++ led_pattern.py
@@ -1,31 +1,36 @@
-import math
+from math import cos, pi, sin, sqrt
 
 
-def generate(LINEAR_LED_COUNT, LINEAR_RADIUS, CIRCULAR_SEGMENT_LED_COUNT, CIRCLULAR_TO_LINEAR_ANGLE):
-    # LINEAR_LED_COUNT = 9  # 7
-    # LINEAR_RADIUS = 1
-    # CIRCLULAR_TO_LINEAR_ANGLE = math.pi / 6  # 25 deg
-    # CIRCULAR_SEGMENT_LED_COUNT = 8  # 7
-
+def generate(linear_led_count, linear_radius, circular_segment_led_count, circular_to_linear_angle):
     linear_leds = [
-        -LINEAR_RADIUS + i * (2 * LINEAR_RADIUS / (LINEAR_LED_COUNT - 1))
-        for i in range(LINEAR_LED_COUNT)
+        -linear_radius + i * (2 * linear_radius / (linear_led_count - 1))
+        for i in range(linear_led_count)
     ]
 
-    circular_segment_angular_size = math.pi * 3 / 2 - 2 * CIRCLULAR_TO_LINEAR_ANGLE
+    circular_segment_angular_size = pi * 3 / 2 - 2 * circular_to_linear_angle
+    circular_segment_radius = sqrt(2) / 2 * linear_radius / sin(circular_segment_angular_size / 2)
 
-    circular_segment_radius = math.sqrt(2) / 2 * LINEAR_RADIUS / math.sin(circular_segment_angular_size / 2)
+    circular_segment_leds = []
+    for i in range(1, circular_segment_led_count + 1):
+        arc_point_x = circular_segment_radius * cos(
+            pi / 4 - circular_segment_angular_size / 2 + i * (
+                circular_segment_angular_size / (circular_segment_led_count + 1)
+            )
+        )
 
-    circular_segment_leds = [
-        (
-            circular_segment_radius * math.cos(
-                math.pi / 4 - circular_segment_angular_size / 2 + i * (circular_segment_angular_size / (CIRCULAR_SEGMENT_LED_COUNT + 1))
-            ) + 1 - circular_segment_radius * math.cos(math.pi / 4 - circular_segment_angular_size / 2),
-            circular_segment_radius * math.sin(
-                math.pi / 4 - circular_segment_angular_size / 2 + i * (circular_segment_angular_size / (CIRCULAR_SEGMENT_LED_COUNT + 1))
-            ) + 1 - circular_segment_radius * math.cos(math.pi / 4 - circular_segment_angular_size / 2)
-        ) for i in range(1, CIRCULAR_SEGMENT_LED_COUNT + 1)
-    ]
+        arc_point_y = circular_segment_radius * sin(
+            pi / 4 - circular_segment_angular_size / 2 + i * (
+                circular_segment_angular_size / (circular_segment_led_count + 1)
+            )
+        )
+
+        arc_circle_center_shift = 1 - circular_segment_radius * cos(
+            pi / 4 - circular_segment_angular_size / 2
+        )
+
+        circular_segment_leds.append(
+            (arc_point_x + arc_circle_center_shift, arc_point_y + arc_circle_center_shift)
+        )
 
     all_circular_segments_leds = [[(x, y), (-x, y), (-x, -y), (x, -y)] for x, y in circular_segment_leds]
 
