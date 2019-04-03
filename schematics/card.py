0609786ce054ec38a2fb256b1e6995c44d68d8cb
Andrei Korigodski <akorigod@gmail.com>
2019-04-03 15:39:22 +0300

    footprints: add new generated footprint for CR2016 holder

--- schematics/card.py
+++ schematics/card.py
@@ -140,7 +140,7 @@ def bicolor_led_matrix(led_template, anodes, cathodes, led_count=None):
         )
 
 
-coin_battery = Part('Device', 'Battery_Cell', footprint='Battery:BatteryHolder_Keystone_3034_1x20mm')
+coin_battery = Part('Device', 'Battery_Cell', footprint='footprints:BatteryHolder_Keystone_3026_1x2016')
 mcu = Part('EFM32LG232F64', 'EFM32LG232F64', footprint='Package_QFP:LQFP-64_10x10mm_P0.5mm')
 lfxo = Part('Device', 'Crystal', value='32768Hz', footprint='Crystal:Crystal_SMD_3215-2Pin_3.2x1.5mm')
 imu = Part('Sensor_Motion', 'LSM6DS3', footprint='footprints:LSM6DS3')
