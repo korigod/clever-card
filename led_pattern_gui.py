015260d78b281da343249dcf242033e9c13059d6
Andrei Korigodski <akorigod@gmail.com>
2019-07-08 01:28:48 +0300

    led_pattern: fix variables names

--- led_pattern_gui.py
+++ led_pattern_gui.py
@@ -6,7 +6,12 @@ from led_pattern import generate
 
 
 def show_values():
-    all_leds = generate(w1.get(), w2.get(), w3.get(), w4.get() / 180 * math.pi)
+    all_leds = generate(
+        linear_led_count.get(),
+        linear_radius.get(),
+        circular_segment_led_count.get(),
+        circular_to_linear_angle.get() / 180 * math.pi
+    )
     plt.plot([x for x, y in all_leds], [y for x, y in all_leds], 'go')
     plt.axis('equal')
     plt.show()
@@ -14,18 +19,18 @@ def show_values():
 
 if __name__ == '__main__':
     master = Tk()
-    w1 = Scale(master, from_=1, to=21, orient=HORIZONTAL)
-    w1.set(9)
-    w1.pack()
-    w2 = Scale(master, from_=1, to=2, orient=HORIZONTAL)
-    w2.set(1)
-    w2.pack()
-    w3 = Scale(master, from_=1, to=21, orient=HORIZONTAL)
-    w3.set(8)
-    w3.pack()
-    w4 = Scale(master, from_=0, to=130, orient=HORIZONTAL)
-    w4.set(30)
-    w4.pack()
+    linear_led_count = Scale(master, from_=1, to=21, orient=HORIZONTAL)
+    linear_led_count.set(9)
+    linear_led_count.pack()
+    linear_radius = Scale(master, from_=1, to=2, orient=HORIZONTAL)
+    linear_radius.set(1)
+    linear_radius.pack()
+    circular_segment_led_count = Scale(master, from_=1, to=21, orient=HORIZONTAL)
+    circular_segment_led_count.set(8)
+    circular_segment_led_count.pack()
+    circular_to_linear_angle = Scale(master, from_=0, to=130, orient=HORIZONTAL)
+    circular_to_linear_angle.set(30)
+    circular_to_linear_angle.pack()
     Button(master, text='Show', command=show_values).pack()
 
     mainloop()
