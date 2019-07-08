64dd8e73c305645e80ced99c7cafcf1a4126d928
Andrei Korigodski <akorigod@gmail.com>
2019-07-08 21:54:54 +0300

    firmware: update statuses of tasks in idle hook
    It's useful for debugging, but introduces jitter in task arbitration and
    delay times. It looks like the update takes up to one millisecond. Maybe
    it would be better to move it to a separate task and call it less
    frequently.

--- firmware/src/debug_hooks.c
+++ firmware/src/debug_hooks.c
@@ -1,6 +1,12 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+TaskStatus_t taskStatuses[10];
+
+void vApplicationIdleHook(void) {
+	uxTaskGetSystemState(taskStatuses, 10, NULL);
+}
+
 void vApplicationStackOverflowHook(TaskHandle_t xTask, signed char *pcTaskName) {
 	configASSERT(0);
 }
