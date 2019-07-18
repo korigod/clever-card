90bf95ac857c2ddf4ee5e55e7ecabff80a1ec34e
Andrei Korigodski <akorigod@gmail.com>
2019-07-18 04:52:11 +0300

    firmware: fake registers for tests

--- firmware/test/test_leds.c
+++ firmware/test/test_leds.c
@@ -3,20 +3,14 @@
 #include "leds.h"
 
 #include "em_gpio.h"
-// #include "mock_em_gpio.h"
-// #include "mock_timer.h"
-// #include "mock_FreeRTOSConfig.h"
-// #include "mock_FreeRTOS.h"
-// #include "mock_task.h"
 
-#include "FreeRTOSConfig.h"
-#include "FreeRTOS.h"
-#include "task.h"
 
 FAKE_VALUE_FUNC(TickType_t, xTaskGetTickCount);
 FAKE_VOID_FUNC(vTaskDelayUntil, TickType_t * const, const TickType_t);
 FAKE_VOID_FUNC(triggerLedsUpdateCycle);
-FAKE_VOID_FUNC(GPIO_PinModeSet, GPIO_Port_TypeDef, unsigned int, GPIO_Mode_TypeDef, unsigned int);
+
+GPIO_TypeDef gpio;
+GPIO_TypeDef * gpioBase = &gpio;
 
 void setUp(void) {
 }
@@ -26,22 +20,10 @@ void tearDown(void) {
 
 void test_enableCathode_portF(void) {
 	enableCathode(5);
-
-	TEST_ASSERT_EQUAL(1, GPIO_PinModeSet_fake.call_count);
-
-	TEST_ASSERT_EQUAL(gpioPortF,        GPIO_PinModeSet_fake.arg0_val);
-	TEST_ASSERT_EQUAL(5,                GPIO_PinModeSet_fake.arg1_val);
-	TEST_ASSERT_EQUAL(gpioModeWiredAnd, GPIO_PinModeSet_fake.arg2_val);
-	TEST_ASSERT_EQUAL(0,                GPIO_PinModeSet_fake.arg3_val);
+	TEST_ASSERT_EQUAL_HEX32(0x8 << 5 * 4, gpio.P[gpioPortF].MODEL);
 }
 
 void test_enableCathode_portE(void) {
 	enableCathode(8);
-
-	TEST_ASSERT_EQUAL(1, GPIO_PinModeSet_fake.call_count);
-
-	TEST_ASSERT_EQUAL(gpioPortE,        GPIO_PinModeSet_fake.arg0_val);
-	TEST_ASSERT_EQUAL(8,                GPIO_PinModeSet_fake.arg1_val);
-	TEST_ASSERT_EQUAL(gpioModeWiredAnd, GPIO_PinModeSet_fake.arg2_val);
-	TEST_ASSERT_EQUAL(0,                GPIO_PinModeSet_fake.arg3_val);
+	TEST_ASSERT_EQUAL_HEX32(0x8 << (8 - 8) * 4, gpio.P[gpioPortE].MODEH);
 }
