3a0014ad1b47d06a6414fdd978a6e3bedef69701
Andrei Korigodskii <akorigod@gmail.com>
2019-08-06 23:11:04 +0300

    firmware: add function to disable LEDs

--- firmware/src/leds.c
+++ firmware/src/leds.c
@@ -10,11 +10,18 @@ void initLeds(void) {
 	}
 }
 
+void disableLeds(void) {
+	switchOffCathodes();
+
+	for (int i = 0; i < sizeof(ledAnodes) / sizeof(ledAnodes[0]); i++) {
+		GPIO_PinModeSet(ledAnodes[i].port, ledAnodes[i].id, gpioModeDisabled, 0);
+	}
+}
+
 
 void switchOnAnode(struct LedAnode anode) {
 	GPIO->P[anode.port].DOUTSET = 1 << anode.id;
 }
-
 
 void switchOffAnodes(void) {
 	// Anodes are pins 0–5 and 8–10 of port A
--- firmware/src/leds.h
+++ firmware/src/leds.h
@@ -14,6 +14,7 @@ struct LedCathode {
 };
 
 void initLeds(void);
+void disableLeds(void);
 
 void switchOnAnode(struct LedAnode anode);
 void switchOffAnodes(void);
