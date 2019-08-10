552c0d4ae6b6886b59ae4b62f0f9f5f33ddf9df7
Andrei Korigodskii <akorigod@gmail.com>
2019-08-10 11:22:36 +0300

    firmware: add function to switch on next LED

--- firmware/src/leds.c
+++ firmware/src/leds.c
@@ -1,6 +1,7 @@
 #include <string.h>
 
 #include "leds.h"
+#include "led_driver.h"
 
 bool cathodesAreEqual(struct LedCathode a, struct LedCathode b) {
 	return (a.id == b.id) && (a.port == b.port);
@@ -10,4 +11,36 @@ bool cathodesAreEqual(struct LedCathode a, struct LedCathode b) {
 // Must be called from a critical section or protected with mutex
 void latchLedOutputs(void) {
 	memcpy(ledOutputsLatched, ledOutputs, sizeof(ledOutputs));
+}
+
+
+enum SwitchOnNextLedStatus switchOnNextLed(bool loopIndefinitely) {
+	uint8_t previousLedIndex = currentLedIndex;
+
+	do {
+		currentLedIndex++;
+		if (currentLedIndex >= ledCount) {
+			if (loopIndefinitely) {
+				currentLedIndex = 0;
+			} else {
+				currentLedIndex = INVALID_LED;
+				switchOffCathodes();
+				return NO_MORE_LEDS;
+			}
+		}
+		// We iterate to skip LEDs with zero brightness
+	} while (ledOutputsLatched[currentLedIndex] == 0);
+
+	if (previousLedIndex == INVALID_LED ||
+	    !cathodesAreEqual(ledPins[previousLedIndex].cathode,
+	                      ledPins[currentLedIndex].cathode))
+	{
+		switchOffCathodes();
+		switchOnCathode(ledPins[currentLedIndex].cathode);
+	}
+
+	uint16_t ticksToWait = 5 * ledOutputsLatched[currentLedIndex];
+	// setTimerToWaitTicks(ticksToWait);
+	switchOnAnode(ledPins[currentLedIndex].anode);
+	return SUCCESS;
 }
--- firmware/src/leds.h
+++ firmware/src/leds.h
@@ -20,11 +20,21 @@ struct LedPins {
 	struct LedCathode cathode;
 };
 
+enum SwitchOnNextLedStatus {
+	SUCCESS,
+	NO_MORE_LEDS
+};
+
 uint8_t ledOutputsLatched[ledCount];
 uint8_t ledOutputs[ledCount];
+
+#define INVALID_LED UINT8_MAX
+uint8_t currentLedIndex = INVALID_LED;
 
 bool cathodesAreEqual(struct LedCathode a, struct LedCathode b);
 
 void latchLedOutputs(void);
+
+enum SwitchOnNextLedStatus switchOnNextLed(bool loopIndefinitely);
 
 #endif /* LEDS_H_INC */
