8dd0f3613029372141a933c1735715533d2c9787
Andrei Korigodskii <akorigod@gmail.com>
2020-01-31 02:27:46 +0300

    firmware: traverse nicely and smoothly through all the leds

--- firmware/src/led_control.c
+++ firmware/src/led_control.c
@@ -1,9 +1,13 @@
+#include <math.h>
+
 #include "FreeRTOSConfig.h"
 #include "FreeRTOS.h"
 #include "task.h"
 
 #include "board.h"
 #include "leds.h"
+
+#define LED_FPS 25
 
 
 void traverseLedsInArray(
@@ -27,17 +31,40 @@ void traverseLedsInArray(
 }
 
 
+void smoothlyTraverseLedsInArray(
+	RgLedId_t arrayToTraverse[],
+	size_t arraySize,
+	bool reverseTraverseDirection,
+	TickType_t * previousWakeTimePtr,
+	uint32_t millisecondsToKeepEachLedOn
+) {
+	for (uint32_t frame = 0; frame <= (arraySize - 1) * millisecondsToKeepEachLedOn * LED_FPS / 1000; frame++) {
+		vTaskDelayUntil(previousWakeTimePtr, pdMS_TO_TICKS(1000 / LED_FPS));
+		clearLeds();
+		float currentLedId = frame * 1000.0f / (float)LED_FPS / (float)millisecondsToKeepEachLedOn;
+		if (reverseTraverseDirection) {
+			currentLedId = (arraySize - 1) - currentLedId;
+		}
+		for (int i = 0; i < arraySize; i++) {
+			ledOutputs[rgLeds[arrayToTraverse[i]].red] = 5 * fmaxf(2 - fabsf(i - currentLedId), 0);
+		}
+
+		latchLedOutputs();
+	}
+}
+
+
 void ledControlTask(void * pvParameters) {
 	TickType_t previousWakeTime = xTaskGetTickCount();
 	const uint32_t timeToKeepLedOn = 100;
 	while (1) {
-		traverseLedsInArray(topLeftArcLeds, sizeof(topLeftArcLeds), false, &previousWakeTime, timeToKeepLedOn);
-		traverseLedsInArray(verticalLeds, sizeof(verticalLeds), true, &previousWakeTime, timeToKeepLedOn);
-		traverseLedsInArray(bottomLeftArcLeds, sizeof(bottomLeftArcLeds), false, &previousWakeTime, timeToKeepLedOn);
-		traverseLedsInArray(horizontalLeds, sizeof(horizontalLeds), false, &previousWakeTime, timeToKeepLedOn);
-		traverseLedsInArray(bottomRightArcLeds, sizeof(bottomRightArcLeds), false, &previousWakeTime, timeToKeepLedOn);
-		traverseLedsInArray(verticalLeds, sizeof(verticalLeds), false, &previousWakeTime, timeToKeepLedOn);
-		traverseLedsInArray(topRightArcLeds, sizeof(topRightArcLeds), false, &previousWakeTime, timeToKeepLedOn);
-		traverseLedsInArray(horizontalLeds, sizeof(horizontalLeds), true, &previousWakeTime, timeToKeepLedOn);
+		smoothlyTraverseLedsInArray(topLeftArcLeds, sizeof(topLeftArcLeds), false, &previousWakeTime, timeToKeepLedOn);
+		smoothlyTraverseLedsInArray(verticalLeds, sizeof(verticalLeds), true, &previousWakeTime, timeToKeepLedOn);
+		smoothlyTraverseLedsInArray(bottomLeftArcLeds, sizeof(bottomLeftArcLeds), false, &previousWakeTime, timeToKeepLedOn);
+		smoothlyTraverseLedsInArray(horizontalLeds, sizeof(horizontalLeds), false, &previousWakeTime, timeToKeepLedOn);
+		smoothlyTraverseLedsInArray(bottomRightArcLeds, sizeof(bottomRightArcLeds), false, &previousWakeTime, timeToKeepLedOn);
+		smoothlyTraverseLedsInArray(verticalLeds, sizeof(verticalLeds), false, &previousWakeTime, timeToKeepLedOn);
+		smoothlyTraverseLedsInArray(topRightArcLeds, sizeof(topRightArcLeds), false, &previousWakeTime, timeToKeepLedOn);
+		smoothlyTraverseLedsInArray(horizontalLeds, sizeof(horizontalLeds), true, &previousWakeTime, timeToKeepLedOn);
 	}
 }
