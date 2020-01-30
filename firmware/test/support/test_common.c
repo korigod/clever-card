8b7955e846ad62875dcddcf9cdbaa0e23b864c3e
Andrei Korigodskii <akorigod@gmail.com>
2020-01-30 03:32:52 +0300

    firmware: fix SWO configuration for tests on MCU

--- firmware/test/support/test_common.c
+++ firmware/test/support/test_common.c
@@ -1,10 +1,6 @@
 #include "test_common.h"
 
 #ifdef TEST_TARGET
-	#include "swo.h"
 	#include "retargetio.h"
-
-	void suiteSetUp(void) {
-		setupSWOForPrint();
-	}
+	#include "swo.h"
 #endif
--- firmware/test/support/test_common.h
+++ firmware/test/support/test_common.h
@@ -1,8 +1,4 @@
 #ifndef TEST_COMMON_H_INC
 #define TEST_COMMON_H_INC
 
-#ifdef TEST_TARGET
-	void suiteSetUp(void);
-#endif
-
 #endif /* TEST_COMMON_H_INC */
