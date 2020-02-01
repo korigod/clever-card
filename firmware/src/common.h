f6d69797df470df0a5d3be08576a8d63999ab3b7
Andrei Korigodskii <akorigod@gmail.com>
2020-02-01 19:56:59 +0300

    firmware: define min and max functions in common.h

--- firmware/src/common.h
+++ firmware/src/common.h
@@ -3,6 +3,18 @@
 
 #include <stdio.h>
 
+#define min(a,b) __extension__ ({ \
+	__typeof__ (a) _a = (a); \
+	__typeof__ (b) _b = (b); \
+	_a < _b ? _a : _b; \
+})
+
+#define max(a,b) __extension__ ({ \
+	__typeof__ (a) _a = (a); \
+	__typeof__ (b) _b = (b); \
+	_a > _b ? _a : _b; \
+})
+
 
 #ifdef TEST
 	#ifdef TEST_TARGET
